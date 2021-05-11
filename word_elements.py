2480efdbac3ebe2bbadd551056d8a447107de59a
rodriguemsb <rodriguesmsb@gmail.com>
2021-05-11 11:52:38 -0400

    create a plat class

--- word_elements.py
+++ word_elements.py
@@ -59,4 +59,13 @@ class Coin(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(img,(tile_size // 2, tile_size // 2))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+
+class Plataform(pygame.sprite.Sprite):
+    def __init__(self, x, y, tile_size):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load("img/plataform.png")
+        self.image = pygame.transform.scale(img,(tile_size, tile_size //2 ))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
         
